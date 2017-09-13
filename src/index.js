24bbbbcc4476ac52eacc8cd17a092802ee87bd42
therewillbecode <tomw08@gmail.com>
2017-09-13 14:24:41 +0100

    Initialise redux store in app root

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 /* eslint-env browser */
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { render } from "react-dom";
 import App from "./App";
 import registerServiceWorker from "../service_worker/registerServiceWorker";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import rootReducer from "./reducers/index";
+
+const store = createStore(rootReducer);
+
+render(
+  <Provider store={store}>
+    <div />
+  </Provider>,
+  document.getElementById("root")
+);
+
 registerServiceWorker();
