ddf3123f14a7e9a0182f53844a7a893926931b8e
therewillbecode <tomw08@gmail.com>
2017-09-14 00:31:52 +0100

    Changed auth0 callback url to loading route

--- src/auth/authConfig.js
+++ src/auth/authConfig.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: "therewillbecode.auth0.com",
   clientId: "2yqWzBSv0zfQEstOyMveQBu4Rw3bqbiT",
-  callbackUrl: "http://localhost:3000/assets"
+  callbackUrl: "http://localhost:3000/loading"
 };
