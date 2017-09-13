3ac9f07f5c62479d134169f5f0fbdda115cfa4cc
therewillbecode <tomw08@gmail.com>
2017-09-13 22:28:25 +0100

    Pass isAuthenticated prop to AppContainer

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import { connect } from "react-redux";
 
 import { isAuthenticated } from "./selectors/index";
@@ -7,10 +7,12 @@ import "./App.css";
 
 class AppContainer extends Component {
   render() {
+    const { isAuthenticated } = this.props;
+
     return (
       <Router history={history} component={App}>
         <div>
-          <AppHeader />
+          <AppHeader isAuthenticated={isAuthenticated} />
           <Route path="/" render={props => <App auth={auth} {...props} />} />
           <Route path="/home" render={props => <Home {...props} />} />
           <Route path="/assets" render={props => <Home {...props} />} />
