783558113c1ad8962404150c98774bf44b962728
therewillbecode <tomw08@gmail.com>
2017-09-13 19:48:34 +0100

    Created authentication HOC
    This is a combination of 9 commits.
    Added redux-mock-store as project dependency
    
    Added initial test for authentication HOC
    
    Added react-test-rendered as project depenedency
    
    Fixed typo
    
    Added snapshot test for auth HOC
    
    Added initial logic for auth HOC
    
    Added more tests for rendering of auth HOC
    
    Added lifecycle methods for pushing history on auth HOC
    
    Made tests pass for HOC

---
+++ src/containers/HOC/RequireAuth.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+export class AuthenticatedComponent extends Component {
+  componentWillMount() {
+    if (!this.props.authenticated) {
+      this.props.history.replace("/");
+    }
+  }
+
+  componentWillUpdate(nextProps) {
+    if (!nextProps.authenticated) {
+      this.props.history.replace("/");
+    }
+  }
+
+  render() {
+    const ComposedComponent = this.props.composedComponent;
+    return (
+      <div className="authenticated">
+        {this.props.authenticated ? (
+          <ComposedComponent {...this.props} />
+        ) : null}
+      </div>
+    );
+  }
+}
+
+export default function RequireAuth(ComposedComponent) {
+  const mapStateToProps = state => ({
+    authenticated: state.get("auth").get("isAuthenticated"),
+    composedComponent: ComposedComponent
+  });
+
+  return connect(mapStateToProps)(AuthenticatedComponent);
+}
---
+++ src/containers/HOC/__test__/RequireAuth.test.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import { Provider } from "react-redux";
+import renderer from "react-test-renderer";
+import configureStore from "redux-mock-store";
+import createHistory from "history/createBrowserHistory";
+import { fromJS } from "immutable";
+
+import RequireAuth, { AuthenticatedComponent } from "../RequireAuth";
+
+const Component = () => <div />;
+Component.displayName = "CustomComponent";
+const mockStore = configureStore([]);
+
+describe("HOC", () => {
+  const RequireAuthComponent = RequireAuth(Component);
+  const initialState = fromJS({ auth: { isAuthenticated: true } });
+  let wrapper = null;
+
+  beforeEach(() => {
+    wrapper = mount(
+      <Provider store={mockStore(initialState)}>
+        <RequireAuthComponent />
+      </Provider>
+    );
+  });
+
+  it("should return a component", () => {
+    expect(wrapper.find("AuthenticatedComponent")).toHaveLength(1);
+  });
+
+  it("should pass correct props", () => {
+    const tree = renderer.create(<wrapper />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("rendering", () => {
+  let history = null;
+
+  describe("is authenticated", () => {
+    history = createHistory();
+    const wrapper = shallow(
+      <AuthenticatedComponent
+        composedComponent={Component}
+        authenticated
+        history={history}
+      />
+    );
+
+    it("should render the passed component", () => {
+      expect(wrapper.find("CustomComponent")).toHaveLength(1);
+    });
+  });
+
+  describe("is not authenticated", () => {
+    const wrapper = shallow(
+      <AuthenticatedComponent
+        composedComponent={Component}
+        authenticated={false}
+        history={history}
+      />
+    );
+
+    it("should not render the passed component", () => {
+      expect(wrapper.find("CustomComponent")).toHaveLength(0);
+    });
+  });
+});
