3e063e226533987dae4aaac89fd626e98f75b330
therewillbecode <tomw08@gmail.com>
2017-09-13 18:14:13 +0100

    Tests for asset filter selector now pass

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -4,3 +4,15 @@ export const assetsSelector = state => state.get("assets");
 
 export const findAssetById = (assets, searchId) =>
   assets.find(({ id }) => id === searchId);
+
+export const filterAssetsByState = (assets, filter) => {
+  switch (filter) {
+    case "received":
+      return assets.filter(({ state }) => state === "received");
+    case "dispatched":
+      return assets.filter(({ state }) => state === "dispatched");
+
+    default:
+      return assets;
+  }
+};
