c15d467e5fd44ce42b4d27d1b50262197a8e2e20
therewillbecode <tomw08@gmail.com>
2017-09-13 23:07:46 +0100

    Added Assets component

---
+++ src/components/Assets.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Assets extends Component {
+  render() {
+    const { assets } = this.props;
+
+    return (
+      <div>
+        <h2>Welcome to the Assets page</h2>
+      </div>
+    );
+  }
+}
+
+export default Assets;
