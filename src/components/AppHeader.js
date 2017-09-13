cf8d8a2aa2b1133cf3c8fd284a376d1b6d6f2fca
therewillbecode <tomw08@gmail.com>
2017-09-13 22:40:52 +0100

    Added Link to home in AppHeader

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,27 +3,15 @@ import { Link } from "react-router-dom";
 
 class AppHeader extends Component {
   render() {
-    const { isAuthenticated } = this.props;
+    const { isAuthenticated, auth } = this.props;
 
     return (
       <div>
-        <button
-          bsStyle="primary"
-          className="btn-margin"
-          onClick={this.goTo.bind(this, "home")}
-        >
-          Home
-        </button>
-        {!isAuthenticated ? (
-          <button bsStyle="primary" className="btn-margin" onClick={login}>
-            Log In
-          </button>
-        ) : null}
+        <Link to="/home">Home</Link>
+        {!isAuthenticated ? <button onClick={auth.login}>Log In</button> : null}
         {isAuthenticated ? (
           <div>
-            <button bsStyle="primary" className="btn-margin" onClick={logout}>
-              Log Out
-            </button>
+            <button onClick={auth.logout}>Log Out</button>
             <Link to="/assets">Assets</Link>
           </div>
         ) : null}
