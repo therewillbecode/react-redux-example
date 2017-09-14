71164b90eafb03d8e06c6f22603641f1e953c335
therewillbecode <tomw08@gmail.com>
2017-09-14 15:48:59 +0100

    Removed inlkine style from div

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div style={{ width: "100%", height: "100%" }}>App</div>;
+    return <div>App</div>;
   }
 }
 
