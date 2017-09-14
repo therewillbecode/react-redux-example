a527c602e168198b5684adab92b678869b753015
therewillbecode <tomw08@gmail.com>
2017-09-14 14:56:37 +0100

    Styled Nav bar using semantic ui

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,21 +1,45 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import {
+  Sidebar,
+  Segment,
+  Button,
+  Menu,
+  Image,
+  Icon,
+  Header
+} from "semantic-ui-react";
+
 class AppHeader extends Component {
   render() {
     const { isAuthenticated, login, logout } = this.props;
 
     return (
-      <div>
-        <Link to="/">Home</Link>
-        {!isAuthenticated ? <button onClick={login}>Log In</button> : null}
+      <Sidebar as={Menu} direction="top" visible inverted>
+        <Menu.Item name="home">
+          <Icon name="home" />
+          <Link to="/">Home</Link>
+        </Menu.Item>
         {isAuthenticated ? (
-          <div>
-            <button onClick={logout}>Log Out</button>
+          <Menu.Item name="assets">
+            <Icon name="barcode" />
             <Link to="/assets">Assets</Link>
-          </div>
+          </Menu.Item>
         ) : null}
-      </div>
+        {!isAuthenticated ? (
+          <Menu.Item onClick={login} name="Login">
+            <Icon name="sign in" />
+            Login
+          </Menu.Item>
+        ) : null}
+        {isAuthenticated ? (
+          <Menu.Item onClick={logout} name="Logout">
+            <Icon name="sign out" />
+            Logout
+          </Menu.Item>
+        ) : null}
+      </Sidebar>
     );
   }
 }
