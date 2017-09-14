9fedbbdf551609e292d413fad615a6c7a6a1cbe6
therewillbecode <tomw08@gmail.com>
2017-09-14 18:30:57 +0100

    Styled navbar

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -17,29 +17,41 @@ class AppHeader extends Component {
     const { isAuthenticated, login, logout, history } = this.props;
 
     return (
-      <Sidebar as={Menu} direction="top" visible inverted>
-        <Menu.Item link onClick={() => history.push("/")} name="title">
-          <h1>Supply Chain Project</h1>
-        </Menu.Item>
-        {isAuthenticated ? (
-          <Menu.Item link onClick={() => history.push("/assets")} name="assets">
-            <Icon name="barcode" />
-            Assets
+      <div style={{ height: "5vh" }}>
+        <Sidebar.Pushable
+          as={Menu}
+          direction="top"
+          width="thin"
+          visible
+          inverted
+        >
+          <Menu.Item link onClick={() => history.push("/")} name="title">
+            <h1>Supply Chain Project</h1>
           </Menu.Item>
-        ) : null}
-        {!isAuthenticated ? (
-          <Menu.Item link position="right" onClick={login} name="Login">
-            <Icon name="sign in" />
-            Login
-          </Menu.Item>
-        ) : null}
-        {isAuthenticated ? (
-          <Menu.Item link position="right" onClick={logout} name="Logout">
-            <Icon name="sign out" />
-            Logout
-          </Menu.Item>
-        ) : null}
-      </Sidebar>
+          {isAuthenticated ? (
+            <Menu.Item
+              link
+              onClick={() => history.push("/assets")}
+              name="assets"
+            >
+              <Icon name="barcode" />
+              Assets
+            </Menu.Item>
+          ) : null}
+          {!isAuthenticated ? (
+            <Menu.Item link position="right" onClick={login} name="Login">
+              <Icon name="sign in" />
+              Login
+            </Menu.Item>
+          ) : null}
+          {isAuthenticated ? (
+            <Menu.Item link position="right" onClick={logout} name="Logout">
+              <Icon name="sign out" />
+              Logout
+            </Menu.Item>
+          ) : null}
+        </Sidebar.Pushable>
+      </div>
     );
   }
 }
