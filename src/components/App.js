5f17f56b9ab7a0bd094206e0609fa1c26a817fa4
therewillbecode <tomw08@gmail.com>
2017-09-15 02:57:44 +0100

    Added text to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import { Icon } from "semantic-ui-react";
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <h2>Automated Asset Tracking. </h2>
+        <Icon size="massive" name="cubes" />
+      </div>
+    );
   }
 }
 
