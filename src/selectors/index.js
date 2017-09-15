0365af89b6e3040ddcb85d0b5e7f02f9341094a1
therewillbecode <tomw08@gmail.com>
2017-09-15 21:22:52 +0100

    Selector tests now pass

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -5,7 +5,11 @@ export const authSelector = state => state.auth;
 export const findAssetById = (assets, searchId) =>
   assets.find(({ id }) => id === searchId);
 
-export const filterAssets = (assets, predicate) => assets.filter(predicate);
+export const filterAssetsByStatus = (assets, status) =>
+  assets.filter(a => a.status === status);
+
+export const filterAssetsBySubstring = (assets, key, substring) =>
+  substring ? assets.filter(a => a[key].includes(substring)) : assets;
 
 export const isAuthenticated = state =>
   authSelector(state).get("isAuthenticated");
