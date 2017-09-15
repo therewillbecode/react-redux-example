b1789f6fc69eff90cd792a355adb8fb8a464b5d2
therewillbecode <tomw08@gmail.com>
2017-09-15 23:41:28 +0100

    Use object literals in action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,49 +1,37 @@
 import * as types from "./types";
 import cuid from "cuid";
 
-export function receiveAsset(name, comment) {
-  return {
-    type: types.RECEIVE_ASSET,
-    id: cuid(),
-    name,
-    timestamp: Date.now(),
-    comment
-  };
-}
+export const receiveAsset = (name, comment) => ({
+  type: types.RECEIVE_ASSET,
+  id: cuid(),
+  name,
+  timestamp: Date.now(),
+  comment
+});
 
-export function dispatchAsset(id) {
-  return {
-    type: types.DISPATCH_ASSET,
-    id,
-    timestamp: Date.now()
-  };
-}
+export const dispatchAsset = id => ({
+  type: types.DISPATCH_ASSET,
+  id,
+  timestamp: Date.now()
+});
 
-export function editAssetName(id, newName) {
-  return {
-    type: types.EDIT_ASSET_NAME,
-    id,
-    newName
-  };
-}
+export const editAssetName = (id, newName) => ({
+  type: types.EDIT_ASSET_NAME,
+  id,
+  newName
+});
 
-export function editAssetComment(id, newComment) {
-  return {
-    type: types.EDIT_ASSET_COMMENT,
-    id,
-    newComment
-  };
-}
+export const editAssetComment = (id, newComment) => ({
+  type: types.EDIT_ASSET_COMMENT,
+  id,
+  newComment
+});
 
-export function authSuccess(idToken) {
-  return {
-    type: types.AUTH_SUCCESS,
-    idToken
-  };
-}
+export const authSuccess = idToken => ({
+  type: types.AUTH_SUCCESS,
+  idToken
+});
 
-export function loggedOut() {
-  return {
-    type: types.LOGGED_OUT
-  };
-}
+export const loggedOut = () => ({
+  type: types.LOGGED_OUT
+});
