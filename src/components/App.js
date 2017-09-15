4db6076663036163aaed75f7715c6659d5c65124
therewillbecode <tomw08@gmail.com>
2017-09-15 10:25:53 +0100

    Add img to home route

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+import { Icon, Image } from "semantic-ui-react";
+import containersImg from "./containers.jpg";
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <h2>Automated Asset Tracking. </h2>
+
+        <img
+          src={{
+            position: "absolute",
+            width: "100vh",
+            height: "100vh",
+            zIndex: "0"
+          }}
+          src={containersImg}
+        />
+
+        <Icon size="massive" name="cubes" />
+      </div>
+    );
   }
 }
 
