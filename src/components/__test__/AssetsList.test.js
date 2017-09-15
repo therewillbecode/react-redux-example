bc0925c1fe781bb85099c8da54198f282f598ff4
therewillbecode <tomw08@gmail.com>
2017-09-15 19:51:25 +0100

    Pass prop in snapshot test

--- src/components/__test__/AssetsList.test.js
+++ src/components/__test__/AssetsList.test.js
@@ -4,10 +4,21 @@ import ReactShallowRenderer from "react-test-renderer/shallow";
 
 import AssetsList from "../AssetsList";
 
+const assets = [
+  {
+    id: "kj48",
+    name: "pfs",
+    status: "dispatched",
+    receivedTimestamp: "34245",
+    dispatchedTimestamp: "5235",
+    comment: "Lorem"
+  }
+];
+
 describe("AssetsList", () => {
   it("renders correctly", () => {
     const renderer = new ReactShallowRenderer();
-    renderer.render(<AssetsList />);
+    renderer.render(<AssetsList assets={assets} />);
     const tree = renderer.getRenderOutput();
     expect(tree).toMatchSnapshot();
   });
