698e71a593db39300c4a4d034c9fa2f78dff8692
therewillbecode <tomw08@gmail.com>
2017-09-15 22:23:16 +0100

    Updated test suite for action creators

--- src/actions/__test__/index.test.js
+++ src/actions/__test__/index.test.js
@@ -6,36 +6,26 @@ describe("actions", () => {
   it("should create an action for received asset", () => {
     const id = "gi4uH53s";
     const name = "car";
-    const timestamp = "1505310201";
-    const comment = "off road";
-    const expectedAction = {
-      type: types.RECEIVE_ASSET,
-      id,
-      name,
-      timestamp,
-      comment
-    };
+    const comment = "lorem ipsum";
+    const action = actions.receiveAsset(id, name, comment);
 
-    expect(actions.receiveAsset(id, name, comment)).toEqual(expectedAction);
+    expect(action.type).toEqual(types.RECEIVE_ASSET);
+    expect(typeof action.id).toEqual("string");
+    expect(typeof action.timestamp).toEqual("number");
   });
 
   it("should create an action to dispatch asset", () => {
     const id = "gi4uH53s";
-    const timestamp = "1505310201";
+    const action = actions.dispatchAsset(id);
 
-    const expectedAction = {
-      type: types.DISPATCH_ASSET,
-      id,
-      timestamp
-    };
-
-    expect(actions.dispatchAsset(id)).toEqual(expectedAction);
+    expect(action.type).toEqual(types.DISPATCH_ASSET);
+    expect(typeof action.id).toEqual("string");
+    expect(typeof action.timestamp).toEqual("number");
   });
 
   it("should create an action to edit asset name", () => {
     const id = "gi4uH53s";
     const newName = "truck";
-
     const expectedAction = {
       type: types.EDIT_ASSET_NAME,
       id,
@@ -48,7 +38,6 @@ describe("actions", () => {
   it("should create an action to edit asset comment", () => {
     const id = "gi4uH53s";
     const newComment = "on road";
-
     const expectedAction = {
       type: types.EDIT_ASSET_COMMENT,
       id,
@@ -56,5 +45,23 @@ describe("actions", () => {
     };
 
     expect(actions.editAssetComment(id, newComment)).toEqual(expectedAction);
+  });
+
+  it("should create an authSuccess action", () => {
+    const idToken = "dfgd434f21";
+    const expectedAction = {
+      type: types.AUTH_SUCCESS,
+      idToken
+    };
+
+    expect(actions.authSuccess(idToken)).toEqual(expectedAction);
+  });
+
+  it("should create a loggedOut action", () => {
+    const expectedAction = {
+      type: types.LOGGED_OUT
+    };
+
+    expect(actions.loggedOut()).toEqual(expectedAction);
   });
 });
