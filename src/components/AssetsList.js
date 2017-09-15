512480ad7da2056226be8538bdd9243ebcceab35
therewillbecode <tomw08@gmail.com>
2017-09-15 01:27:10 +0100

    Use map to render cells and header cells in AssetList table

--- src/components/AssetsList.js
+++ src/components/AssetsList.js
@@ -1,39 +1,37 @@
+import cuid from "cuid";
 import React from "react";
 import { Table } from "semantic-ui-react";
+
+const assetAttributes = {
+  id: "ID",
+  name: "Name",
+  status: "Status",
+  receivedTimestamp: "Time Received",
+  dispatchedTimestamp: "Time Dispatched",
+  comment: "Comments"
+};
 
 const AssetsList = ({ assets, navigateToAsset }) => (
   <Table celled padded inverted selectable>
     <Table.Header>
       <Table.Row>
-        <Table.HeaderCell textAlign="center">ID</Table.HeaderCell>
-        <Table.HeaderCell textAlign="center">Name</Table.HeaderCell>
-        <Table.HeaderCell textAlign="center">Status</Table.HeaderCell>
-        <Table.HeaderCell textAlign="center">Time Received</Table.HeaderCell>
-        <Table.HeaderCell textAlign="center">Time Dispatched</Table.HeaderCell>
-        <Table.HeaderCell textAlign="center">Comments</Table.HeaderCell>
+        {Object.values(assetAttributes).map(key => (
+          <Table.HeaderCell key={key} textAlign="center">
+            <h3>{key}</h3>
+          </Table.HeaderCell>
+        ))}
       </Table.Row>
     </Table.Header>
-
     <Table.Body>
-      {assets.map(
-        ({
-          id,
-          name,
-          status,
-          receivedTimestamp,
-          dispatchedTimestamp,
-          comment
-        }) => (
-          <Table.Row key={id} onClick={() => navigateToAsset(id)}>
-            <Table.Cell textAlign="center">{id}</Table.Cell>
-            <Table.Cell textAlign="center">{name}</Table.Cell>
-            <Table.Cell textAlign="center">{status}</Table.Cell>
-            <Table.Cell textAlign="center">{receivedTimestamp}</Table.Cell>
-            <Table.Cell textAlign="center">{dispatchedTimestamp}</Table.Cell>
-            <Table.Cell textAlign="left">{comment}</Table.Cell>
-          </Table.Row>
-        )
-      )}
+      {assets.map(asset => (
+        <Table.Row key={asset.id} onClick={() => navigateToAsset(asset.id)}>
+          {Object.keys(assetAttributes).map(attribute => (
+            <Table.Cell key={cuid()} textAlign="center">
+              <h5>{asset[attribute]}</h5>
+            </Table.Cell>
+          ))}
+        </Table.Row>
+      ))}
     </Table.Body>
   </Table>
 );
