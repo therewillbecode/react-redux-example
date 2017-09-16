73c12c142a4ff7a26dfea069dc8bbd19562a1dee
therewillbecode <tomw08@gmail.com>
2017-09-16 13:57:15 +0100

    Added enzyme tests to assert form handlers called on submit

--- src/components/__test__/ReceiveAssetForm.test.js
+++ src/components/__test__/ReceiveAssetForm.test.js
@@ -1,6 +1,9 @@
 /* eslint-disable */
 import React from "react";
 import ReactShallowRenderer from "react-test-renderer/shallow";
+import sinon from "sinon";
+import { shallow, mount } from "enzyme";
+import { Form } from "semantic-ui-react";
 
 import ReceiveAssetForm from "../ReceiveAssetForm";
 
@@ -10,5 +13,19 @@ describe("ReceiveAssetForm", () => {
     renderer.render(<ReceiveAssetForm />);
     const tree = renderer.getRenderOutput();
     expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Calls handler props", () => {
+  let spy = null;
+
+  beforeEach(() => {
+    spy = new sinon.spy();
+  });
+
+  it("should call handleSubmit when form is submitted", () => {
+    const wrapper = shallow(<ReceiveAssetForm handleSubmit={spy} />);
+    wrapper.find(Form).simulate("submit");
+    expect(spy.calledOnce).toBe(true);
   });
 });
