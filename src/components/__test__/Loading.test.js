e08c754a2bd7223ed26ab6ec00d55e61d931c6ea
therewillbecode <tomw08@gmail.com>
2017-09-16 14:55:42 +0100

    Test that handleAuth is called on loading screen to parse token from hash
    Removed comment

--- src/components/__test__/Loading.test.js
+++ src/components/__test__/Loading.test.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
 import React from "react";
 import ReactShallowRenderer from "react-test-renderer/shallow";
+import sinon from "sinon";
+import { mount } from "enzyme";
 
 import Loading from "../Loading";
 
@@ -10,5 +12,18 @@ describe("Loading", () => {
     renderer.render(<Loading />);
     const tree = renderer.getRenderOutput();
     expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Calls handler props", () => {
+  let spy = null;
+
+  beforeEach(() => {
+    spy = new sinon.spy();
+  });
+
+  it("should call handleAuthentication prop on mount", () => {
+    const wrapper = mount(<Loading handleAuthentication={spy} />);
+    expect(spy.calledOnce).toBe(true);
   });
 });
